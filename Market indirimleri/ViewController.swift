240ee39f6468843f4aac166af579dd24911f11e3
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-04-23 20:29:36 +0400

    Initial Commit

---
+++ Market indirimleri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Market indirimleri
+//
+//  Created by İlyas Abiyev on 4/23/20.
+//  Copyright © 2020 İlyas Abiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
